af8c44a4d6e4be30a76858b38f21d29b35b41316
pradyumna-scstechsol <pradyumna.kanakapura@scstechsol.com>
2022-12-16 17:27:42 +0530

    changed the git link

--- calculator.py
+++ calculator.py
@@ -7,6 +7,6 @@ def add(first_num , second_num):
 def subtract(first_num , second_num):
     return first_num - second_num    
 
-print(add(2,2))
+print(add(5,2))
 
-print(subtract(4,2))
+print(subtract(7,2))
