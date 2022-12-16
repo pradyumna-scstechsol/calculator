fb9f66a01dc25f411f77fd02bfb8798f960a231b
pradyumna-scstechsol <pradyumna.kanakapura@scstechsol.com>
2022-12-16 15:23:43 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+
+
+def add(first_num , second_num):
+    return first_num + second_num
+
+
+def subtract(first_num , second_num):
+    return first_num - second_num    
+
+print(add(2,2))
+
+print(subtract(4,2))
